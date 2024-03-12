54bfad7fb2864b9deee0a0244e81710243acdd49
Buhary Asmath <asmath.info@gmail.com>
2024-03-12 07:23:05 +0530

    sign up functinality added

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ const collection =require("./config")
 
 const app = express();
 
+//convert data into json
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+
+
 app.set("view engine", "ejs");
 
 //static files
@@ -17,6 +23,18 @@ app.get("/", (req, res) => {
 app.get("/signup", (req, res) => {
   res.render("signup");
 });
+
+//register new users
+app.post("/signup",async(req,res)=>{
+const data ={
+  name : req.body.username,
+  password : req.body.password,
+}
+
+
+const userdata = await collection.insertMany(data);
+console.log(userdata)
+})
 
 const port = 5000;
 app.listen(port, () => {
