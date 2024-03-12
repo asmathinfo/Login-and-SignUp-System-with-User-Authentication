2a312729bdcd6bed161ccf7317b70696e5ce864e
Buhary Asmath <asmath.info@gmail.com>
2024-03-12 07:52:55 +0530

    login functinoality and homepage routes

--- src/index.js
+++ src/index.js
@@ -22,6 +22,10 @@ app.get("/signup", (req, res) => {
   res.render("signup");
 });
 
+// app.get("home", (req, res) => {
+//   res.render("home");
+// });
+
 //register new users
 app.post("/signup", async (req, res) => {
   const data = {
@@ -35,13 +39,36 @@ app.post("/signup", async (req, res) => {
     res.send("User Already Exists, Please Choose differnet Username");
   } else {
     //hash the pw using bcrypt
-    const saltRounds=10;
-    const hashedPassword= await bcrypt.hash(data.password,saltRounds);
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(data.password, saltRounds);
 
     data.password = hashedPassword;
-    
+
     const userdata = await collection.insertMany(data);
     console.log(userdata);
+  }
+});
+
+// Login User
+app.post("/login", async (req, res) => {
+  try {
+    const check = await collection.findOne({ name: req.body.username });
+    if (!check) {
+      res.send("Username cannot Found");
+    }
+
+    //comapre the password from db with plain text
+    const isPasswordMatch = await bcrypt.compare(
+      req.body.password,
+      check.password
+    );
+    if (isPasswordMatch) {
+      res.render("home");
+    } else {
+      req.send("Wrong Password");
+    }
+  } catch {
+    res.send("Wrong Details");
   }
 });
 
